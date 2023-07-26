91857e16d6f60dc080a342d4319f999a3733ecbf
Juan Diego Quintero <juan.quintero@blankfactor.com>
2023-07-26 16:43:03 -0500

    Progress in feature-card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Button } from "./components/Button";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardSubtitle,
+  CardTitle,
+} from "./components/Card";
 
 export function App() {
   return (
     <>
       <h1>Bootstrap and mui</h1>
       <Button variant="outline" />
+      <Card>
+        <CardTitle>Melo</CardTitle>
+        <CardSubtitle>Subtitulo</CardSubtitle>
+        <CardHeader>Bienvenido</CardHeader>
+        <CardBody>Hola mundo</CardBody>
+      </Card>
     </>
   );
 }
